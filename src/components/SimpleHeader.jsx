4fc322f548179c9a39466b0f5c0ca2af0c24c2a7
Runqiu Bao <runqiu.bao@tum.de>
2026-01-24 10:14:59 +0900

    fix cv pdf link

--- src/components/SimpleHeader.jsx
+++ src/components/SimpleHeader.jsx
@@ -1,6 +1,7 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 import profileImg from '../resources/DSC00726.jpg';
+import cvPdf from '../resources/CV_RUNQIU_BAO_ENGLISH.pdf'
 
 
 const SimpleHeader = ({ darkMode, setDarkMode }) => {
@@ -46,7 +47,7 @@ const SimpleHeader = ({ darkMode, setDarkMode }) => {
     },
     {
       name: 'CV',
-      url: '${import.meta.env.BASE_URL}resources/CV_RUNQIU_BAO_ENGLISH.pdf',
+      url: cvPdf,
       icon: (
         <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
